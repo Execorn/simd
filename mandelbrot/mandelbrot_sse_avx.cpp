547497ea4637e5594ec8c0cc49778107039b5e00
Execorn <n.biryukov.04@gmail.com>
2022-04-05 18:10:23 +0300

    increase fps, smaller window

--- mandelbrot/mandelbrot_sse_avx.cpp
+++ mandelbrot/mandelbrot_sse_avx.cpp
@@ -11,8 +11,8 @@ const float O_x = -1.325f,
             O_y = 0;
 
 
-const int   WINDOW_WIDTH  = 1920;
-const int   WINDOW_HEIGHT = 1080;
+const int   WINDOW_WIDTH  = 800;
+const int   WINDOW_HEIGHT = 600;
 
 
 int main() {
@@ -25,17 +25,17 @@ int main() {
     
     const int    max_points  = 256;
     const float  dx    = 1/800.f, dy = 1/800.f;
-    const __m256 r2Max = _mm256_set1_ps (100.f);
+
+    const __m256 maxR_vector = _mm256_set1_ps (100.f);
     const __m256 _255  = _mm256_set1_ps (255.f);
     const __m256 _76543210 = _mm256_set_ps  (7.f, 6.f, 5.f, 4.f, 3.f, 2.f, 1.f, 0.f);
-    
-    const __m256 nmax  = _mm256_set1_ps (max_points);
+    const __m256 maxP_vector  = _mm256_set1_ps (max_points);
 
     float xC = 0.f, yC = 0.f, scale = 1.f;
 
-    uint8_t global_program_flag = 1;
-    while (global_program_flag) {
-        if (GetAsyncKeyState (VK_ESCAPE)) global_program_flag = 0;
+    uint8_t do_draw_cycle = 1;
+    while (do_draw_cycle) {
+        if (GetAsyncKeyState (VK_ESCAPE)) do_draw_cycle = 0;
         
         if (txGetAsyncKeyState (VK_RIGHT)) xC    += dx * (txGetAsyncKeyState (VK_SHIFT)? 100.f : 10.f);
         if (txGetAsyncKeyState (VK_LEFT))  xC    -= dx * (txGetAsyncKeyState (VK_SHIFT)? 100.f : 10.f);
@@ -45,8 +45,8 @@ int main() {
         if (txGetAsyncKeyState ('Z'))      scale -= dx * (txGetAsyncKeyState (VK_SHIFT)? 100.f : 10.f);
 
         #pragma omp parallel for
-        for (int iy = 0; iy < 1080 * global_program_flag; iy++) {
-            if (GetAsyncKeyState (VK_ESCAPE)) global_program_flag = 0;
+        for (int iy = 0; iy < 1080 * do_draw_cycle; iy++) {
+            if (GetAsyncKeyState (VK_ESCAPE)) do_draw_cycle = 0;
 
             float y0 = ( ((float) iy - (float) WINDOW_HEIGHT / 2) * dy) * scale + O_y + yC;
 
@@ -60,17 +60,17 @@ int main() {
                 
                 __m256i N = _mm256_setzero_si256();
                 
-                uint8_t local_program_flag = 1;
+                uint8_t do_calc_points = 1;
                 for (int n = 0; n < max_points; n++) {
-                    if (local_program_flag) {
+                    if (do_calc_points) {
                         __m256 x2 = _mm256_mul_ps (X, X),
                            y2 = _mm256_mul_ps (Y, Y);
                            
                         __m256 r2 = _mm256_add_ps (x2, y2);
 
-                        __m256 cmp = _mm256_cmp_ps (r2, r2Max, _CMP_LE_OQ);
+                        __m256 cmp = _mm256_cmp_ps (r2, maxR_vector, _CMP_LE_OQ);
                         int mask   = _mm256_movemask_ps (cmp);
-                        if (!mask) local_program_flag = 0;
+                        if (!mask) do_calc_points = 0;
                             
                         N = _mm256_sub_epi32 (N, _mm256_castps_si256 (cmp));
 
@@ -84,7 +84,7 @@ int main() {
                 /*
                 * Filter mask for better point drawing
                 */
-                __m256 I = _mm256_mul_ps (_mm256_sqrt_ps (_mm256_sqrt_ps (_mm256_div_ps (_mm256_cvtepi32_ps (N), nmax))), _255);
+                __m256 I = _mm256_mul_ps (_mm256_sqrt_ps (_mm256_sqrt_ps (_mm256_div_ps (_mm256_cvtepi32_ps (N), maxP_vector))), _255);
 
                 for (int i = 0; i < 8; i++) {
                     int*   pn = (int*)   &N;
