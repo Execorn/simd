4327dd451901ca91453fdb770b059f93cff8137c
Execorn <n.biryukov.04@gmail.com>
2022-04-06 11:56:43 +0300

    multithreading added, syntax fixed, comments added

--- mandelbrot/mandelbrot_sse_avx.cpp
+++ mandelbrot/mandelbrot_sse_avx.cpp
@@ -24,26 +24,26 @@ int main() {
     scr_t scr = (scr_t) *txVideoMemory();
     
     const int    max_points  = 256;
-    const float  dx    = 1/800.f, 
-                 dy    = 1/800.f;
+    const float  dx          = 1/800.f, 
+                 dy          = 1/800.f;
 
     const __m256 maxR_vector = _mm256_set1_ps (100.f);
     const __m256 _255  = _mm256_set1_ps (255.f);
     const __m256 _76543210 = _mm256_set_ps  (7.f, 6.f, 5.f, 4.f, 3.f, 2.f, 1.f, 0.f);
     const __m256 maxP_vector  = _mm256_set1_ps (max_points);
 
-    float xC = 0.f, yC = 0.f, scale = 1.f;
+    float cur_x = 0.f, cur_y = 0.f, scale = 1.f;
 
     uint8_t do_draw_cycle = 1;
     while (do_draw_cycle) {
         if (GetAsyncKeyState (VK_ESCAPE)) do_draw_cycle = 0;
         
-        if (txGetAsyncKeyState (VK_RIGHT)) xC    += dx * (txGetAsyncKeyState (VK_SHIFT)? 50.f : 10.f);
-        if (txGetAsyncKeyState (VK_LEFT))  xC    -= dx * (txGetAsyncKeyState (VK_SHIFT)? 50.f : 10.f);
-        if (txGetAsyncKeyState (VK_DOWN))  yC    -= dy * (txGetAsyncKeyState (VK_SHIFT)? 50.f : 10.f);
-        if (txGetAsyncKeyState (VK_UP))    yC    += dy * (txGetAsyncKeyState (VK_SHIFT)? 0.f : 10.f);
-        if (txGetAsyncKeyState ('A'))      scale += dx * (txGetAsyncKeyState (VK_SHIFT)? 5100.f : 10.f);
-        if (txGetAsyncKeyState ('Z'))      scale -= dx * (txGetAsyncKeyState (VK_SHIFT)? 100.f : 10.f);
+        if (txGetAsyncKeyState (VK_RIGHT)) cur_x    += dx * (txGetAsyncKeyState (VK_SHIFT)? 50.f : 5.f);
+        if (txGetAsyncKeyState (VK_LEFT))  cur_x    -= dx * (txGetAsyncKeyState (VK_SHIFT)? 50.f : 5.f);
+        if (txGetAsyncKeyState (VK_DOWN))  cur_y    -= dy * (txGetAsyncKeyState (VK_SHIFT)? 50.f : 5.f);
+        if (txGetAsyncKeyState (VK_UP))    cur_y    += dy * (txGetAsyncKeyState (VK_SHIFT)? 50.f : 5.f);
+        if (txGetAsyncKeyState ('A'))      scale    += dx * (txGetAsyncKeyState (VK_SHIFT)? 50.f : 5.f);
+        if (txGetAsyncKeyState ('Z'))      scale    -= dx * (txGetAsyncKeyState (VK_SHIFT)? 50.f : 5.f);
 
         #pragma omp parallel for
         for (int iy = 0; iy < WINDOW_HEIGHT * do_draw_cycle; ++iy) {
@@ -52,51 +52,67 @@ int main() {
             /**
             * ! Formula for new y0 coord:
             * ! We are decreasing iy by WINDOW_HEIGHT / 2 to find, range of point's coordinates on the screen
-            * ! 0_y + yC are added to find the actual location based on the previous coordinates
+            * ! 0_y + cur_y are added to find the actual location based on the previous coordinates
             * 
             * ? x0 is calculated inside of the cycle because OpenMP does not allow not constant variable usage in cycle ? 
             */
-            float y0 = (((float) iy - (float) WINDOW_HEIGHT / 2) * dy) * scale + O_y + yC;
+
+            float y0 = (((float) iy - (float) WINDOW_HEIGHT / 2) * dy) * scale + O_y + cur_y;
 
             #pragma omp parallel for
             for (int ix = 0; ix < WINDOW_WIDTH; ix += 8 /*, x0 += dx * 8 * scale*/) { 
-                float x0 = (((float) ix - (float) WINDOW_WIDTH / 2) * dx) * scale + O_x + xC;
-                __m256  X0 = _mm256_add_ps (_mm256_set1_ps (x0), _mm256_mul_ps (_76543210, _mm256_set1_ps (dx * scale)));
-                __m256  Y0 =                _mm256_set1_ps (y0);
+                float x0 = (((float) ix - (float) WINDOW_WIDTH / 2) * dx) * scale + O_x + cur_x;
 
-                __m256  X = X0, 
-                        Y = Y0;
+                /**
+                 * * Formula: *
+                 * ! X0_vector = x0 + dx * 8 * scale
+                 * ? Y0_vector = y0 (doesn't change right away) ?
+                 */
+                __m256  X0_vector = _mm256_add_ps (_mm256_set1_ps (x0), _mm256_mul_ps (_76543210, _mm256_set1_ps (dx * scale)));
+                __m256  Y0_vector =                _mm256_set1_ps (y0);
+
+                __m256  X_vector = X0_vector, 
+                        Y_vector = Y0_vector;
                 
-                __m256i N = _mm256_setzero_si256(); // Setting all elements of N (vector) to zero 
+                __m256i iter_vector = _mm256_setzero_si256(); // Setting all elements of iter_vector to zero 
                 
                 uint8_t do_calc_points = 1;
-                for (int n = 0; n < max_points; n++) {
+                for (int point = 0; point < max_points; ++point) {
                     if (do_calc_points) {
-                        __m256  x2 = _mm256_mul_ps (X, X),
-                                y2 = _mm256_mul_ps (Y, Y);
+                        __m256  x2 = _mm256_mul_ps (X_vector, X_vector),
+                                y2 = _mm256_mul_ps (Y_vector, Y_vector);
                            
                         __m256 r2 = _mm256_add_ps (x2, y2);
-
+                        /**
+                         * ! _CMP_LE_OQ or _CMP_LE_OS is a defined operator <= in AVX ! 
+                         * ? _LE_ means less-equal ?
+                         */
                         __m256 cmp = _mm256_cmp_ps (r2, maxR_vector, _CMP_LE_OQ);
+                        
+                        /**
+                         * ! mask will contain '1' byte, if cmp (vector) contains not-null object, else 0 !
+                         * ? if !mask <=> mask == 0, then all elements in vector cmp are null ?
+                         */
                         int mask   = _mm256_movemask_ps (cmp);
-                        if (!mask) do_calc_points = 0;
+                        if (mask == 0) do_calc_points = 0;
                             
-                        N = _mm256_sub_epi32 (N, _mm256_castps_si256 (cmp));
+                        iter_vector = _mm256_sub_epi32 (iter_vector, _mm256_castps_si256 (cmp));
 
-                        __m256 xy = _mm256_mul_ps (X, Y);
+                        __m256 xy = _mm256_mul_ps (X_vector, Y_vector);
 
-                        X = _mm256_add_ps (_mm256_sub_ps (x2, y2), X0);
-                        Y = _mm256_add_ps (_mm256_add_ps (xy, xy), Y0);
+                        X_vector = _mm256_add_ps (_mm256_sub_ps (x2, y2), X0_vector);
+                        Y_vector = _mm256_add_ps (_mm256_add_ps (xy, xy), Y0_vector);
                     }
                 }
                 
-                /*
-                * Filter mask for better point drawing
-                */
-                __m256 I = _mm256_mul_ps (_mm256_sqrt_ps (_mm256_sqrt_ps (_mm256_div_ps (_mm256_cvtepi32_ps (N), maxP_vector))), _255);
+                /**
+                 * * Filter mask with the special formula *
+                 * ? Filtered mask allows us to make better coloring algorithm ? 
+                 */
+                __m256 I = _mm256_mul_ps (_mm256_sqrt_ps (_mm256_sqrt_ps (_mm256_div_ps (_mm256_cvtepi32_ps (iter_vector), maxP_vector))), _255);
 
                 for (int i = 0; i < 8; i++) {
-                    int*   pn = (int*)   &N;
+                    int*   pn = (int*)   &iter_vector;
                     float* pI = (float*) &I;
 
                     BYTE    c     = (BYTE) pI[i];
--- overlay/overlay.cpp
+++ overlay/overlay.cpp
@@ -10,8 +10,8 @@
 
 typedef RGBQUAD (&scr_t) [600][800];
 
-inline scr_t LoadImage (const char* filename)
-    {
+inline scr_t LoadImage (const char* filename) {
+    // TODO: Fix memory leak (forgetting to clear 2 HDC)
     RGBQUAD* mem = NULL;
     HDC dc = txCreateDIBSection (800, 600, &mem);
     txBitBlt (dc, 0, 0, 0, 0, dc, 0, 0, BLACKNESS);
@@ -24,18 +24,16 @@ inline scr_t LoadImage (const char* filename)
     return (scr_t) *mem;
     }
 
-//-------------------------------------------------------------------------------------------------
+// < Constants for 0/1 bytes and vectors
 
 const char I = 255u,
            Z = 0x80u;
            
-const __m128i   _0 =                  _mm_set_epi8 (0,0,0,0, 0,0,0,0, 0,0,0,0, 0,0,0,0);
-const __m128i _255 = _mm_cvtepu8_epi16 (_mm_set_epi8 (I,I,I,I, I,I,I,I, I,I,I,I, I,I,I,I));
+const __m128i   _0 =                    _mm_set_epi8 (0,0,0,0, 0,0,0,0, 0,0,0,0, 0,0,0,0);  // ! Setting with 0-bytes
+const __m128i _255 = _mm_cvtepu8_epi16 (_mm_set_epi8 (I,I,I,I, I,I,I,I, I,I,I,I, I,I,I,I)); // ? Setting with 1-bytes
 
-//-------------------------------------------------------------------------------------------------
 
-int main()
-    {
+int main() {
     txCreateWindow (800, 600);
     Win32::_fpreset();
     txBegin();
@@ -44,126 +42,124 @@ int main()
     scr_t back  = (scr_t) LoadImage ("images/Table.bmp");
     scr_t scr   = (scr_t) *txVideoMemory();
 
-    for (int n = 0; /*n <= 1000*/; n++)
-        {
+    for (int img = 0; /*img <= 1000*/; ++img) {
         if (GetAsyncKeyState (VK_ESCAPE)) break;
         
-        if (!GetKeyState (VK_SPACE))
-            {
-            for (int y = 0; y < 600; y++) 
-            for (int x = 0; x < 800; x += 4)
-                {
-                //-----------------------------------------------------------------------
-                //       15 14 13 12   11 10  9  8    7  6  5  4    3  2  1  0
-                // fr = [r3 g3 b3 a3 | r2 g2 b2 a2 | r1 g1 b1 a1 | r0 g0 b0 a0]
-                //-----------------------------------------------------------------------
+        if (!GetKeyState (VK_SPACE)) {
+            #pragma omp parallel for
+            for (int y = 0; y < 600; ++y) {
+                #pragma omp parallel for
+                for (int x = 0; x < 800; x += 4) {
+                    //-----------------------------------------------------------------------
+                    //       15 14 13 12   11 10  9  8    7  6  5  4    3  2  1  0
+                    // fr = [r3 g3 b3 a3 | r2 g2 b2 a2 | r1 g1 b1 a1 | r0 g0 b0 a0]
+                    //-----------------------------------------------------------------------
 
-                __m128i fr = _mm_load_si128 ((__m128i*) &front[y][x]);                   // fr = front[y][x]
-                __m128i bk = _mm_load_si128 ((__m128i*) &back [y][x]);
+                    __m128i fr = _mm_load_si128 ((__m128i*) &front[y][x]);                   // fr = front[y][x]
+                    __m128i bk = _mm_load_si128 ((__m128i*) &back [y][x]);
 
-                //-----------------------------------------------------------------------
-                //       15 14 13 12   11 10  9  8    7  6  5  4    3  2  1  0
-                // fr = [a3 r3 g3 b3 | a2 r2 g2 b2 | a1 r1 g1 b1 | a0 r0 g0 b0]
-                //        \  \  \  \    \  \  \  \   xx xx xx xx   xx xx xx xx            
-                //         \  \  \  \    \  \  \  \.
-                //          \  \  \  \    '--+--+--+-------------+--+--+--.
-                //           '--+--+--+------------+--+--+--.     \  \  \  \.
-                //                                  \  \  \  \     \  \  \  \.
-                // FR = [-- -- -- -- | -- -- -- -- | a3 r3 g3 b3 | a2 r2 g2 b2]
-                //-----------------------------------------------------------------------
+                    //-----------------------------------------------------------------------
+                    //       15 14 13 12   11 10  9  8    7  6  5  4    3  2  1  0
+                    // fr = [a3 r3 g3 b3 | a2 r2 g2 b2 | a1 r1 g1 b1 | a0 r0 g0 b0]
+                    //        \  \  \  \    \  \  \  \   xx xx xx xx   xx xx xx xx            
+                    //         \  \  \  \    \  \  \  \.
+                    //          \  \  \  \    '--+--+--+-------------+--+--+--.
+                    //           '--+--+--+------------+--+--+--.     \  \  \  \.
+                    //                                  \  \  \  \     \  \  \  \.
+                    // FR = [-- -- -- -- | -- -- -- -- | a3 r3 g3 b3 | a2 r2 g2 b2]
+                    //-----------------------------------------------------------------------
 
-                __m128i FR = (__m128i) _mm_movehl_ps ((__m128) _0, (__m128) fr);       // FR = (fr >> 8*8)
-                __m128i BK = (__m128i) _mm_movehl_ps ((__m128) _0, (__m128) bk);
+                    __m128i FR = (__m128i) _mm_movehl_ps ((__m128) _0, (__m128) fr);       // FR = (fr >> 8*8)
+                    __m128i BK = (__m128i) _mm_movehl_ps ((__m128) _0, (__m128) bk);
 
-                //-----------------------------------------------------------------------
-                //       15 14 13 12   11 10  9  8    7  6  5  4    3  2  1  0
-                // fr = [a3 r3 g3 b3 | a2 r2 g2 b2 | a1 r1 g1 b1 | a0 r0 g0 b0]
-                //       xx xx xx xx   xx xx xx xx                 /  /   |  |
-                //                                         _______/  /   /   |
-                //            ...   ...     ...           /     ____/   /    |
-                //           /     /       /             /     /       /     |
-                // fr = [-- a1 -- r1 | -- g1 -- b1 | -- a0 -- r0 | -- g0 -- b0]
-                //-----------------------------------------------------------------------
+                    //-----------------------------------------------------------------------
+                    //       15 14 13 12   11 10  9  8    7  6  5  4    3  2  1  0
+                    // fr = [a3 r3 g3 b3 | a2 r2 g2 b2 | a1 r1 g1 b1 | a0 r0 g0 b0]
+                    //       xx xx xx xx   xx xx xx xx                 /  /   |  |
+                    //                                         _______/  /   /   |
+                    //            ...   ...     ...           /     ____/   /    |
+                    //           /     /       /             /     /       /     |
+                    // fr = [-- a1 -- r1 | -- g1 -- b1 | -- a0 -- r0 | -- g0 -- b0]
+                    //-----------------------------------------------------------------------
 
-                fr = _mm_cvtepu8_epi16 (fr);                                               // fr[i] = (WORD) fr[i]
-                FR = _mm_cvtepu8_epi16 (FR);
+                    fr = _mm_cvtepu8_epi16 (fr);                                               // fr[i] = (WORD) fr[i]
+                    FR = _mm_cvtepu8_epi16 (FR);
 
-                bk = _mm_cvtepu8_epi16 (bk);
-                BK = _mm_cvtepu8_epi16 (BK);
+                    bk = _mm_cvtepu8_epi16 (bk);
+                    BK = _mm_cvtepu8_epi16 (BK);
 
-                //-----------------------------------------------------------------------
-                //       15 14 13 12   11 10  9  8    7  6  5  4    3  2  1  0
-                // fr = [-- a1 -- r1 | -- g1 -- b1 | -- a0 -- r0 | -- g0 -- b0]
-                //          |___________________        |___________________
-                //          |     \      \      \       |     \      \      \.
-                // a  = [-- a1 -- a1 | -- a1 -- a1 | -- a0 -- a0 | -- a0 -- a0]
-                //-----------------------------------------------------------------------
+                    //-----------------------------------------------------------------------
+                    //       15 14 13 12   11 10  9  8    7  6  5  4    3  2  1  0
+                    // fr = [-- a1 -- r1 | -- g1 -- b1 | -- a0 -- r0 | -- g0 -- b0]
+                    //          |___________________        |___________________
+                    //          |     \      \      \       |     \      \      \.
+                    // a  = [-- a1 -- a1 | -- a1 -- a1 | -- a0 -- a0 | -- a0 -- a0]
+                    //-----------------------------------------------------------------------
 
-                static const __m128i moveA = _mm_set_epi8 (Z, 14, Z, 14, Z, 14, Z, 14,
-                                                            Z, 6, Z, 6, Z, 6, Z, 6);
-                __m128i a = _mm_shuffle_epi8 (fr, moveA);                                // a [for r0/b0/b0...] = a0...
-                __m128i A = _mm_shuffle_epi8 (FR, moveA);
-                
-                //-----------------------------------------------------------------------
+                    static const __m128i moveA = _mm_set_epi8 (Z, 14, Z, 14, Z, 14, Z, 14,
+                                                               Z, 6,  Z, 6,  Z, 6,  Z, 6);
+                    __m128i a = _mm_shuffle_epi8 (fr, moveA);                                // a [for r0/b0/b0...] = a0...
+                    __m128i A = _mm_shuffle_epi8 (FR, moveA);
+                    
+                    //-----------------------------------------------------------------------
 
-                fr = _mm_mullo_epi16 (fr, a);                                           // fr *= a
-                FR = _mm_mullo_epi16 (FR, A);
+                    fr = _mm_mullo_epi16 (fr, a);                                           // fr *= a
+                    FR = _mm_mullo_epi16 (FR, A);
 
-                bk = _mm_mullo_epi16 (bk, _mm_sub_epi16 (_255, a));                                  // bk *= (255-a)
-                BK = _mm_mullo_epi16 (BK, _mm_sub_epi16 (_255, A));
+                    bk = _mm_mullo_epi16 (bk, _mm_sub_epi16 (_255, a));                                  // bk *= (255-a)
+                    BK = _mm_mullo_epi16 (BK, _mm_sub_epi16 (_255, A));
 
-                __m128i sum = _mm_add_epi16 (fr, bk);                                       // sum = fr*a + bk*(255-a)
-                __m128i SUM = _mm_add_epi16 (FR, BK);
+                    __m128i sum = _mm_add_epi16 (fr, bk);                                       // sum = fr*a + bk*(255-a)
+                    __m128i SUM = _mm_add_epi16 (FR, BK);
 
-                //-----------------------------------------------------------------------
-                //        15 14 13 12   11 10  9  8    7  6  5  4    3  2  1  0
-                // sum = [A1 a1 R1 r1 | G1 g1 B1 b1 | A0 a0 R0 r0 | G0 g0 B0 b0]
-                //         \     \       \     \       \_____\_______\_____\.
-                //          \_____\_______\_____\______________    \  \  \  \.
-                //                                    \  \  \  \    \  \  \  \.
-                // sum = [-- -- -- -- | -- -- -- -- | A1 R1 G1 B1 | A0 R0 G0 B0]
-                //-----------------------------------------------------------------------
+                    //-----------------------------------------------------------------------
+                    //        15 14 13 12   11 10  9  8    7  6  5  4    3  2  1  0
+                    // sum = [A1 a1 R1 r1 | G1 g1 B1 b1 | A0 a0 R0 r0 | G0 g0 B0 b0]
+                    //         \     \       \     \       \_____\_______\_____\.
+                    //          \_____\_______\_____\______________    \  \  \  \.
+                    //                                    \  \  \  \    \  \  \  \.
+                    // sum = [-- -- -- -- | -- -- -- -- | A1 R1 G1 B1 | A0 R0 G0 B0]
+                    //-----------------------------------------------------------------------
 
-                static const __m128i moveSum = _mm_set_epi8 (Z, Z, Z, Z, Z, Z, Z, Z, 
-                                                             15, 13, 11, 9, 7, 5, 3, 1);
-                sum = _mm_shuffle_epi8 (sum, moveSum);                                      // sum[i] = (sium[i] >> 8) = (sum[i] / 256)
-                SUM = _mm_shuffle_epi8 (SUM, moveSum);
-                
-                //-----------------------------------------------------------------------
-                //          15 14 13 12   11 10  9  8    7  6  5  4    3  2  1  0
-                // sum   = [-- -- -- -- | -- -- -- -- | a1 r1 g1 b1 | a0 r0 g0 b0] ->-.
-                // sumHi = [-- -- -- -- | -- -- -- -- | a3 r3 g3 b3 | a2 r2 g2 b2]    |
-                //                                      /  /  /  /    /  /  /  /      V
-                //             .--+--+--+----+--+--+--++--+--+--+----+--+--+--'       |
-                //            /  /  /  /    /  /  /  /    ____________________________/
-                //           /  /  /  /    /  /  /  /    /  /  /  /    /  /  /  /
-                // color = [a3 r3 g3 b3 | a2 r2 g2 b2 | a1 r1 g1 b1 | a0 r0 g0 b0]
-                //-----------------------------------------------------------------------
+                    static const __m128i moveSum = _mm_set_epi8 (Z,  Z,  Z,  Z, Z, Z, Z, Z, 
+                                                                 15, 13, 11, 9, 7, 5, 3, 1);
+                    sum = _mm_shuffle_epi8 (sum, moveSum);                                      // sum[i] = (sium[i] >> 8) = (sum[i] / 256)
+                    SUM = _mm_shuffle_epi8 (SUM, moveSum);
+                    
+                    //-----------------------------------------------------------------------
+                    //          15 14 13 12   11 10  9  8    7  6  5  4    3  2  1  0
+                    // sum   = [-- -- -- -- | -- -- -- -- | a1 r1 g1 b1 | a0 r0 g0 b0] ->-.
+                    // sumHi = [-- -- -- -- | -- -- -- -- | a3 r3 g3 b3 | a2 r2 g2 b2]    |
+                    //                                      /  /  /  /    /  /  /  /      V
+                    //             .--+--+--+----+--+--+--++--+--+--+----+--+--+--'       |
+                    //            /  /  /  /    /  /  /  /    ____________________________/
+                    //           /  /  /  /    /  /  /  /    /  /  /  /    /  /  /  /
+                    // color = [a3 r3 g3 b3 | a2 r2 g2 b2 | a1 r1 g1 b1 | a0 r0 g0 b0]
+                    //-----------------------------------------------------------------------
 
-                __m128i color = (__m128i) _mm_movelh_ps ((__m128) sum, (__m128) SUM);  // color = (sumHi << 8*8) | sum
+                    __m128i color = (__m128i) _mm_movelh_ps ((__m128) sum, (__m128) SUM);  // color = (sumHi << 8*8) | sum
 
-                _mm_store_si128 ((__m128i*) &scr[y][x], color);
+                    _mm_store_si128 ((__m128i*) &scr[y][x], color);
+                    }
+            }    
+        } else {
+            for (int y = 0; y < 600; ++y) {
+                for (int x = 0; x < 800; ++x) {
+                    RGBQUAD* fr = &front[y][x];
+                    RGBQUAD* bk = &back [y][x];
+                    
+                    uint16_t a  = fr->rgbReserved;
+
+                    scr[y][x]   = { (BYTE) ( (fr->rgbBlue  * (a) + bk->rgbBlue  * (255-a)) >> 8 ),
+                                    (BYTE) ( (fr->rgbGreen * (a) + bk->rgbGreen * (255-a)) >> 8 ),
+                                    (BYTE) ( (fr->rgbRed   * (a) + bk->rgbRed   * (255-a)) >> 8 ) };
                 }
-            }
-        else
-            {
-            for (int y = 0; y < 600; y++) 
-            for (int x = 0; x < 800; x++) {
-                RGBQUAD* fr = &front[y][x];
-                RGBQUAD* bk = &back [y][x];
-                
-                uint16_t a  = fr->rgbReserved;
-
-                scr[y][x]   = { (BYTE) ( (fr->rgbBlue  * (a) + bk->rgbBlue  * (255-a)) >> 8 ),
-                                (BYTE) ( (fr->rgbGreen * (a) + bk->rgbGreen * (255-a)) >> 8 ),
-                                (BYTE) ( (fr->rgbRed   * (a) + bk->rgbRed   * (255-a)) >> 8 ) };
-                }
-            }
-                
-        if (!(n % 10)) printf ("\t\r%.0lf", txGetFPS() * 10);
-        txUpdateWindow();
+            }        
         }
-
-    txDisableAutoPause();
+                
+        if (!(img % 10)) printf ("\t\r%.0lf", txGetFPS() * 10);
+        txUpdateWindow();
     }
+    txDisableAutoPause();
+}
     
