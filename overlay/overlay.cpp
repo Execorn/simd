f0bd90ea5205c57f6724e4abb1b2fbe5def2f617
Execorn <n.biryukov.04@gmail.com>
2022-05-23 11:11:04 +0300

    comments updated

--- overlay/overlay.cpp
+++ overlay/overlay.cpp
@@ -11,7 +11,6 @@
 typedef RGBQUAD (&scr_t) [600][800];
 
 inline scr_t LoadImage (const char* filename) {
-    // TODO: Fix memory leak (forgetting to clear 2 HDC)
     RGBQUAD* mem = NULL;
     HDC dc = txCreateDIBSection (800, 600, &mem);
     txBitBlt (dc, 0, 0, 0, 0, dc, 0, 0, BLACKNESS);
@@ -24,86 +23,137 @@ inline scr_t LoadImage (const char* filename) {
     return (scr_t) *mem;
     }
 
+//----------------------------------------------------------------
 // < Constants for 0/1 bytes and vectors
 
-const char I = 255u,
-           Z = 0x80u;
+const char units_only       = 255u,
+           unit_and_zeros   = 0x80u;
            
-const __m128i   _0 =                    _mm_set_epi8 (0,0,0,0, 0,0,0,0, 0,0,0,0, 0,0,0,0);  // ! Setting with 0-bytes
-const __m128i _255 = _mm_cvtepu8_epi16 (_mm_set_epi8 (I,I,I,I, I,I,I,I, I,I,I,I, I,I,I,I)); // ? Setting with 1-bytes
-
+// ! Setting with 0-bytes
+const __m128i only_zeros =                    _mm_set_epi8 (0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0); 
+// ? Setting with 1-bits
+const __m128i only_units = _mm_cvtepu8_epi16 ( _mm_set_epi8(units_only, units_only,
+                                                            units_only, units_only,
+                                                            units_only, units_only,
+                                                            units_only, units_only,
+                                                            units_only, units_only,
+                                                            units_only, units_only,
+                                                            units_only, units_only,
+                                                            units_only, units_only));
+//----------------------------------------------------------------
 
 int main() {
-    txCreateWindow (800, 600);
+    // ! Creating window of size 800x600, WITHOUT DRAWING IT YET
+    txCreateWindow (800, 600); 
     Win32::_fpreset();
     txBegin();
 
-    scr_t front = (scr_t) LoadImage ("images/AskhatCat.bmp");
-    scr_t back  = (scr_t) LoadImage ("images/Table.bmp");
+
+    //----------------------------------------------------------------
+    // < Setting images, getting image video memory
+    scr_t foreground  = (scr_t) LoadImage ("images/AskhatCat.bmp");
+    scr_t background  = (scr_t) LoadImage ("images/Table.bmp");
     scr_t scr   = (scr_t) *txVideoMemory();
+    //----------------------------------------------------------------
+
 
     for (int img = 0; ; ++img) {
-        if (GetAsyncKeyState (VK_ESCAPE)) break;
-        
+        if (GetAsyncKeyState (VK_ESCAPE)) {
+            break;
+        }
+
         if (!GetKeyState (VK_SPACE)) {
             #pragma omp parallel for
             for (int y = 0; y < 600; ++y) {
                 #pragma omp parallel for
                 for (int x = 0; x < 800; x += 4) {
+                    // ! Reading current background pixel, current foreground pixel, 
+                    // ? frontal_byte = foreground[y][x]
+                    __m128i frontal_byte    = _mm_load_si128 ((__m128i*) &foreground[y][x]); 
+                    __m128i background_byte = _mm_load_si128 ((__m128i*) &background [y][x]);
 
-                    __m128i fr = _mm_load_si128 ((__m128i*) &front[y][x]);                   // fr = front[y][x]
-                    __m128i bk = _mm_load_si128 ((__m128i*) &back [y][x]);
 
-        
-                    __m128i FR = (__m128i) _mm_movehl_ps ((__m128) _0, (__m128) fr);       // FR = (fr >> 8*8)
-                    __m128i BK = (__m128i) _mm_movehl_ps ((__m128) _0, (__m128) bk);
+                    // ! Putting 2 upper value of image(s) byte to 2 lower values of zero-vector, saving 
+                    // ? shifting frontal byte 64 times, so we get '0000000.....00000(frontal_byte)'
+                    __m128i frontal_shifted     = (__m128i) _mm_movehl_ps ((__m128) only_zeros, (__m128)    frontal_byte);   
+                    __m128i backfround_shifted  = (__m128i) _mm_movehl_ps ((__m128) only_zeros, (__m128) background_byte);
 
-                    fr = _mm_cvtepu8_epi16 (fr);                                               // fr[i] = (WORD) fr[i]
-                    FR = _mm_cvtepu8_epi16 (FR);
 
-                    bk = _mm_cvtepu8_epi16 (bk);
-                    BK = _mm_cvtepu8_epi16 (BK);
+                    // ! Zero-extend frontal_bytes 
+                    // ? frontal_byte[i] = (BYTE) frontal_byte[i]
+                    frontal_byte    = _mm_cvtepu8_epi16 (frontal_byte);                                             
+                    frontal_shifted = _mm_cvtepu8_epi16 (frontal_shifted);
 
-                    static const __m128i moveA = _mm_set_epi8 (Z, 14, Z, 14, Z, 14, Z, 14,
-                                                               Z, 6,  Z, 6,  Z, 6,  Z, 6);
-                    __m128i a = _mm_shuffle_epi8 (fr, moveA);                                // a [for r0/b0/b0...] = a0...
-                    __m128i A = _mm_shuffle_epi8 (FR, moveA);
+
+                    // ! Zero-extend background_bytes 
+                    // ? background_byte[i] = (BYTE) background_byte[i]
+                    background_byte     = _mm_cvtepu8_epi16 (background_byte);
+                    backfround_shifted  = _mm_cvtepu8_epi16 (backfround_shifted);
+
+
+                    // ! FIRST MASK TO SHUFFLE
+                    // ? shuffling like that: frontal_shuffled [for r0/b0/b0...] = a0...
+                    static const __m128i shuffle_control_mask = _mm_set_epi8 (  unit_and_zeros, 14, 
+                                                                                unit_and_zeros, 14, 
+                                                                                unit_and_zeros, 14, 
+                                                                                unit_and_zeros, 14,
+                                                                                unit_and_zeros, 6,  
+                                                                                unit_and_zeros, 6,  
+                                                                                unit_and_zeros, 6,  
+                                                                                unit_and_zeros, 6);
                     
-                    fr = _mm_mullo_epi16 (fr, a);                                           // fr *= a
-                    FR = _mm_mullo_epi16 (FR, A);
+                    // < actually shuffling by bytes
+                    __m128i frontal_shuffled         = _mm_shuffle_epi8 (frontal_byte,    shuffle_control_mask);     
+                    __m128i frontal_shifted_shuffled = _mm_shuffle_epi8 (frontal_shifted, shuffle_control_mask);
 
-                    bk = _mm_mullo_epi16 (bk, _mm_sub_epi16 (_255, a));                                  // bk *= (255-a)
-                    BK = _mm_mullo_epi16 (BK, _mm_sub_epi16 (_255, A));
+                    // ! calculating alpha-value (opacity) of new image
+                    // ? frontal_byte *= frontal_shuffled    
+                    frontal_byte    = _mm_mullo_epi16 (frontal_byte, frontal_shuffled);                           
+                    frontal_shifted = _mm_mullo_epi16 (frontal_shifted, frontal_shifted_shuffled);
 
-                    __m128i sum = _mm_add_epi16 (fr, bk);                                       // sum = fr*a + bk*(255-a)
-                    __m128i SUM = _mm_add_epi16 (FR, BK);
+                    // ! now getting background byte opacity with multiplication
+                    // ? background_byte *= (255-frontal_shuffled)
+                    background_byte     = _mm_mullo_epi16 (background_byte,     _mm_sub_epi16 (only_units, frontal_shuffled));                                 
+                    backfround_shifted  = _mm_mullo_epi16 (backfround_shifted,  _mm_sub_epi16 (only_units, frontal_shifted_shuffled));
 
-                    static const __m128i moveSum = _mm_set_epi8 (Z,  Z,  Z,  Z, Z, Z, Z, Z, 
+                    __m128i sum = _mm_add_epi16 (frontal_byte, background_byte);                                       // sum = frontal_byte*frontal_shuffled + background_byte*(255-frontal_shuffled)
+                    __m128i SUM = _mm_add_epi16 (frontal_shifted, backfround_shifted);
+
+                    static const __m128i shuffle_control_sum = _mm_set_epi8 (unit_and_zeros,  
+                                                                unit_and_zeros,  
+                                                                unit_and_zeros,  
+                                                                unit_and_zeros, 
+                                                                unit_and_zeros, 
+                                                                unit_and_zeros, 
+                                                                unit_and_zeros, 
+                                                                unit_and_zeros, 
                                                                  15, 13, 11, 9, 7, 5, 3, 1);
-                    sum = _mm_shuffle_epi8 (sum, moveSum);                                      // sum[i] = (sium[i] >> 8) = (sum[i] / 256)
-                    SUM = _mm_shuffle_epi8 (SUM, moveSum);
+                    sum = _mm_shuffle_epi8 (sum, shuffle_control_sum);                                      // sum[i] = (sium[i] >> 8) = (sum[i] / 256)
+                    SUM = _mm_shuffle_epi8 (SUM, shuffle_control_sum);
                 
                     __m128i color = (__m128i) _mm_movelh_ps ((__m128) sum, (__m128) SUM);  // color = (sumHi << 8*8) | sum
 
                     _mm_store_si128 ((__m128i*) &scr[y][x], color);
-                    }
+                }
             }    
         } else {
             for (int y = 0; y < 600; ++y) {
                 for (int x = 0; x < 800; ++x) {
-                    RGBQUAD* fr = &front[y][x];
-                    RGBQUAD* bk = &back [y][x];
+                    RGBQUAD* frontal_byte = &foreground[y][x];
+                    RGBQUAD* background_byte = &background [y][x];
                     
-                    uint16_t a  = fr->rgbReserved;
+                    uint16_t frontal_shuffled  = frontal_byte->rgbReserved;
 
-                    scr[y][x]   = { (BYTE) ( (fr->rgbBlue  * (a) + bk->rgbBlue  * (255-a)) >> 8 ),
-                                    (BYTE) ( (fr->rgbGreen * (a) + bk->rgbGreen * (255-a)) >> 8 ),
-                                    (BYTE) ( (fr->rgbRed   * (a) + bk->rgbRed   * (255-a)) >> 8 ) };
+                    scr[y][x]   = { (BYTE) ( (frontal_byte->rgbBlue  * (frontal_shuffled) + background_byte->rgbBlue  * (255-frontal_shuffled)) >> 8 ),
+                                    (BYTE) ( (frontal_byte->rgbGreen * (frontal_shuffled) + background_byte->rgbGreen * (255-frontal_shuffled)) >> 8 ),
+                                    (BYTE) ( (frontal_byte->rgbRed   * (frontal_shuffled) + background_byte->rgbRed   * (255-frontal_shuffled)) >> 8 ) };
                 }
             }        
         }
                 
-        if (!(img % 10)) printf ("\t\r%.0lf", txGetFPS() * 10);
+        if (img % 10 == 0) {
+            printf ("\t\r%.0lf", txGetFPS() * 10);
+        }    
         txUpdateWindow();
     }
     txDisableAutoPause();
